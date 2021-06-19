82bcbe509f5e4598eacd5798769d64d0cec5c81b
Mtchuikov <71787586+Mtchuikov@users.noreply.github.com>
2021-06-19 13:18:06 +0300

    Update models.py

--- bot/db/models.py
+++ bot/db/models.py
@@ -1,38 +1,33 @@
 # -*- coding: utf-8 -*-
 
-from sqlalchemy import Column, String, Integer, Float
+from sqlalchemy import Column, String, Integer, Float, Boolean
 from sqlalchemy.ext.declarative import declarative_base
 
 
 def wrapped_models(Base: declarative_base):
 
 
-    class ValidatorTemporary(Base):
-        __tablename__ = 'validator_temporary'
-
-        id = Column(Integer, primary_key=True, autoincrement=True)
-        address = Column(String, unique=True)
-        score = Column(Integer)
-        response_time = Column(Float)
-        responses = Column(Integer)
-
-
-    class ValidatorStatic(Base):
+    class ValidatorData(Base):
         __tablename__ = 'validator_static'
 
         id = Column(Integer, primary_key=True, autoincrement=True)
+        is_active = Column(Boolean)
         address = Column(String, unique=True)
-        commission = Column(Integer)
 
-        total_score = Column(Integer)
+        score = Column(Integer)
         daily_score_changes = Column(Integer)
         weekly_score_changes = Column(Integer)
         monthly_score_changes = Column(Integer)
 
-        current_response_time = Column(Float)
+        response_time = Column(Float)
         average_response_time = Column(Float)
 
-        total_stake = Column(Float)
+        responses = Column(Integer)
+        daily_responses_changes = Column(Integer)
+        weekly_responses_changes = Column(Integer)
+        monthly_responses_changes = Column(Integer)
+
+        current_stake = Column(Float)
         daily_stake_changes = Column(Float)
         weekly_stake_changes = Column(Float)
         monthly_stake_changes = Column(Float)
@@ -45,6 +40,4 @@ def wrapped_models(Base: declarative_base):
         text = Column(String)
 
 
-    return ValidatorTemporary, ValidatorStatic, Leaderboard
-
-    
+    return ValidatorData, Leaderboard
